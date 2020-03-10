3e56119273c5b1c7e70024b63a1ab8d000b0463e
Dmitriy Dulkin <37985913+AmazingSuka@users.noreply.github.com>
2020-03-11 00:49:46 +0400

    [UPDATE] Connect to Google SMPT server and configure contact form.

--- src/send.php
+++ src/send.php
@@ -1,13 +1,40 @@
 <?php
-    $name = $_POST['name'];
-    $theme = $_POST['theme'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    
-    mail(
-        'dulckin.dim@yandex.ru', 
-        "$theme", 
-        "$message", 
-        "From: {$email}"
-    );
-?>
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require_once __DIR__ . '/../vendor/phpmailer/phpmailer/src/Exception.php';
+require_once __DIR__ . '/../vendor/phpmailer/phpmailer/src/SMTP.php';
+
+try {
+    $mail = new PHPMailer(true);
+
+    $mail->isSMTP();
+    $mail->Host        = 'smtp.gmail.com';
+    $mail->SMTPAuth    = true;
+    $mail->SMTPSecure  = 'tls';
+    $mail->Debugoutput = 'html';
+    $mail->SMTPDebug   = 2;
+    $mail->Port        = 587;
+    $mail->Username    = 'dulkin2010j@gmail.com';
+    $mail->Password    = 'jxpynmydszddnxvt';
+
+    $mail->setFrom($_POST['email']);
+    $mail->addAddress('dulckin.dim@yandex.ru');
+    $mail->Subject = $_POST['theme'] . '. Пользователь: ' . $_POST['name'];
+    $mail->Body = $_POST['message'] . "\nДанное сообщение было отправленно с dulyanich.herokuapp.com";
+
+    $mail->send();
+
+    $url = $_SERVER['HTTP_HOST'] . rtrim(dirname($_SERVER['PHP_SELF']), '/\\') . '/index.php';
+
+    if (headers_sent()) {
+        echo ("<script>location.href='$url'</script>");
+    } else {
+        header("Location: $url");
+    }
+} catch (Throwable $tr) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
